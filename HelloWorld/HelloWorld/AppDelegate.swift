d6a96e06d9ab504cdfbb07a2c5816358ed729d22
Manan Sharma <manansharma@MacBook-Pro.local>
2016-05-04 08:11:54 +0530

    adding files

--- HelloWorld/HelloWorld/AppDelegate.swift
+++ HelloWorld/HelloWorld/AppDelegate.swift
@@ -41,7 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    func applicationWillTerminate1(application: UIApplication) {
+    func applicationWillTerminate2(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
